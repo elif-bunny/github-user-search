5c394b235dcefd973ec128111c91d825c6a9b8fa
elif-bunny <robdev91@gmail.com>
2021-03-09 02:13:40 -0500

    add profile page

--- src/views/pages/profile/index.vue
+++ src/views/pages/profile/index.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
-    {{ 100 }}
-  </div>
+  <v-card class="mx-auto" max-width="1000" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4" v-if="user.name !== null">
+          {{ user.name }}
+        </div>
+        <v-list-item-title class="headline mb-1">
+          <a :href="user.html_url" target="_blank">{{ user.login }}</a>
+        </v-list-item-title>
+        <div style="height: 5px"></div>
+        <v-list-item-subtitle v-if="user.bio !== null">
+          {{ user.bio }}
+        </v-list-item-subtitle>
+        <v-list-item-content> </v-list-item-content>
+        <v-list-item-content>
+          {{ user.followers }} followers, {{ user.following }} following
+        </v-list-item-content>
+        <v-list-item-content>
+          {{ user.public_repos }} public repositories,
+          {{ user.public_gists }} public gists
+        </v-list-item-content>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey">
+        <v-img :src="user.avatar_url"> </v-img>
+      </v-list-item-avatar>
+    </v-list-item>
+    <a
+      :href="user.url"
+      class="d-flex mr-3 mb-5"
+      style="justify-content: flex-end"
+      target="_blank"
+      >API</a
+    >
+  </v-card>
 </template>
 <script>
 import { mapActions, mapState } from "vuex";
@@ -17,10 +49,7 @@ export default {
   },
 
   methods: {
-    ...mapActions("github", [
-      "getUser",
-      "setLoading",
-    ]),
+    ...mapActions("github", ["getUser", "setLoading"]),
 
     async loadUser() {
       try {
@@ -37,7 +66,7 @@ export default {
     },
 
     openUserLink() {
-      console.log('open user link')
+      console.log("open user link");
     },
   },
 
